4bbabcdb21b9c53d21f0372e57ca461ed577aca8
hemanthio <121523327+hemanthio@users.noreply.github.com>
2024-01-26 22:29:41 +0530

    added navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { MdSearch } from "react-icons/md";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+  <div className='my-0 mx-auto max-w-screen-md h-screen  '>
+    <div className='m-5  justify-center relative flex items-center '>
+    <MdSearch size="xl" color='gray' className='absolute w-6 translate-x-28' />
+    <div className='text-white rounded-full bg-search  px-14 py-2 pr-16'>BANOTH HEMANTH SAI</div>
+    </div>
+   
+  </div>
+   </>
   )
 }
 
